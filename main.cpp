53cd898d60edeb83f4f409d2de73af43491254cc
Jonas Johan Solsvik <jonas.solsvik@gmail.com>
2018-05-08 14:47:32 +0200

    un-compressing vertices, normals and texturecoordinates

--- main.cpp
+++ main.cpp
@@ -1,28 +1,19 @@
 #include <cstdio>
 #include <string>
 #include <iostream>
+#include <cstdint>
+
+using u8 = std::uint8_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
 
 #include <tiny_obj_loader/tiny_obj_loader.h>
-
-
-struct Vertex
-{
-    float         x,y,z;
-    float         nx,ny,nz;
-    float         u,v;
-    unsigned char r=255,g=255,b=255,a=255;
-};
-
-struct Triangle 
-{
-    unsigned long a,b,c;
-};
 
 
 int main() { 
 
     // @doc The code below is a write off from https://github.com/syoyo/tinyobjloader - 07.05.2018
-    constexpr char inputfile[] = "../assets/obj/cube/cube.obj";
+    constexpr char inputfile[] = "../assets/obj/moriknob/testObj.obj";
 
     auto attrib    = tinyobj::attrib_t{};
     auto shapes    = std::vector<tinyobj::shape_t>{};
@@ -59,38 +50,116 @@ int main() {
     // We just fill the texels and normals into the preallocated vertex slots 
     // below. - JSolsvik 08.05.2018
 
+
+    struct Vertex
+    {
+        float         x,y,z;
+        float         nx,ny,nz;
+        float         u,v;
+        unsigned char r=255,g=255,b=255,a=255;
+    };
+
+    struct Triangle 
+    {
+        unsigned long a,b,c;
+    };
+
     std::vector<Vertex> overkillVertices;
     overkillVertices.resize(attrib.vertices.size());
     std::vector<Triangle> overkillTriangles;
 
     for (auto shape: shapes)
     {
-        auto mesh = shape.mesh;
-        printf("shape.name = %s\n", shape.name.data());
-        printf("Size of shape.indices: %lu\n", static_cast<unsigned long>(mesh.indices.size()));
+        auto& mesh = shape.mesh;
+        printf("\n\nshape.name = %s\n", shape.name.data());
+        printf("Size of shape.indices: %lu\n", static_cast<u64>(mesh.indices.size()));
 
         std::size_t indexOffset = 0;
 
-        for (std::size_t faceIdx = 0; faceIdx < mesh.num_face_vertices.size(); ++faceIdx) 
+        for (u64 faceIdx = 0; faceIdx < mesh.num_face_vertices.size(); ) 
         {
-            std::size_t faceVertexCount = mesh.num_face_vertices[faceIdx];
-            
-            if (faceVertexCount != 3) {
-                std::cerr << "ERR: VertexNum != 3\n";
+            if (mesh.num_face_vertices[faceIdx] != 3) {
+                std::cerr << "ERR: mesh.num_face_vertices != 3\n";
                 exit(0);
             }
 
-            printf("face[%lu].a = %d/%d/%d  ", faceIdx, mesh.indices[faceIdx + 0].vertex_index
-                                                      , mesh.indices[faceIdx + 0].normal_index
-                                                      , mesh.indices[faceIdx + 0].texcoord_index);
+            auto& indices = mesh.indices;
+            auto& vertices = attrib.vertices;
+            auto& normals = attrib.normals;
+            auto& texcoords = attrib.texcoords;
+           // auto& colors = attrib.colors; This is just an extension of the obj format.
 
-            printf("face[%lu].b = %d/%d/%d  ", faceIdx, mesh.indices[faceIdx + 1].vertex_index
-                                                      , mesh.indices[faceIdx + 1].normal_index
-                                                      , mesh.indices[faceIdx + 1].texcoord_index);
 
-            printf("face[%lu].c = %d/%d/%d\n", faceIdx, mesh.indices[faceIdx + 2].vertex_index
-                                                      , mesh.indices[faceIdx + 2].normal_index
-                                                      , mesh.indices[faceIdx + 2].texcoord_index);
+            auto& ia = indices[faceIdx + 0]; 
+            auto& ib = indices[faceIdx + 1];
+            auto& ic = indices[faceIdx + 2];
+
+            // Uncompress vertices
+            {
+                const u8 Stride = 3;
+                printf("\n"
+                       "va = %f %f %f\n", vertices[ia.vertex_index * Stride + 0], 
+                                          vertices[ia.vertex_index * Stride + 1], 
+                                          vertices[ia.vertex_index * Stride + 2]);
+
+                printf("vb = %f %f %f\n",  vertices[ib.vertex_index * Stride + 0], 
+                                           vertices[ib.vertex_index * Stride + 1], 
+                                           vertices[ib.vertex_index * Stride + 2]);
+
+                printf("vc =%f %f %f\n", vertices[ic.vertex_index * Stride + 0], 
+                                         vertices[ic.vertex_index * Stride + 1], 
+                                         vertices[ic.vertex_index * Stride + 2]);
+            }
+
+            // Uncompress normals
+            {
+                const u8 Stride = 3;
+                printf("\n"
+                       "na = %f %f %f\n", normals[ia.normal_index * Stride + 0], 
+                                          normals[ia.normal_index * Stride + 1], 
+                                          normals[ia.normal_index * Stride + 2]);
+
+                printf("nb = %f %f %f\n",  normals[ib.normal_index * Stride + 0], 
+                                           normals[ib.normal_index * Stride + 1], 
+                                           normals[ib.normal_index * Stride + 2]);
+
+                printf("nc =%f %f %f\n", normals[ic.normal_index * Stride + 0], 
+                                         normals[ic.normal_index * Stride + 1], 
+                                         normals[ic.normal_index * Stride + 2]);
+            }
+
+            // Uncompress texture coordinates
+            {
+                const u8 Stride = 2;
+                printf("\n"
+                       "ta = %f %f\n", texcoords[ia.texcoord_index * Stride + 0], 
+                                       texcoords[ia.texcoord_index * Stride + 1]);
+
+                printf("tb = %f %f\n", texcoords[ib.texcoord_index * Stride + 0], 
+                                       texcoords[ib.texcoord_index * Stride + 1]);
+
+                printf("tc = %f %f\n", texcoords[ic.texcoord_index * Stride + 0], 
+                                       texcoords[ic.texcoord_index * Stride + 1]);
+
+            }
+
+            printf("face[%lu] a = %d/%d/%d  ", faceIdx, ia.vertex_index
+                                                      , ia.normal_index
+                                                      , ia.texcoord_index);
+
+            printf(" b =  %d/%d/%d  ", ib.vertex_index
+                                     , ib.normal_index
+                                     , ib.texcoord_index);
+
+            printf(" c =  %d/%d/%d\n", ic.vertex_index
+                                     , ic.normal_index
+                                     , ic.texcoord_index);
+
+
+            if (faceIdx > 2)
+                exit(0);
+
+            faceIdx += 3;
         }
     }
 
