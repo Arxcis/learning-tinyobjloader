49d85b84018f38da22ac95cd475b88b4aba3eed8
Jonas Johan Solsvik <jonas.solsvik@gmail.com>
2018-05-08 20:01:52 +0200

    getting material data

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ static bool FileExists(const std::string& abs_filename) {
 static std::string handleArguments(const int argc, const char** argv) 
 {
     if (argc > 2 ) {
-        std::cout << "ONLY 0 or 1 argument allowed \n";
+        std::cout << "Usage: ./main <filepath>\n";
         exit(1);
     }
     std::string objfilepath;
@@ -149,13 +149,15 @@ int main(const int argc, const char** argv) {
         printf("Mesh.number_of_indices: %lu\n", static_cast<u64>(mesh.indices.size()));
         printf("Mesh.number_of_faces: %lu\n",   static_cast<u64>(mesh.num_face_vertices.size()));
         
-        
+                
 
         const u8 TriangleStride = 3;
         for (u64 faceIdx = 0; faceIdx < mesh.num_face_vertices.size(); faceIdx += TriangleStride)
         {
 
-            if (mesh.num_face_vertices[faceIdx] != 3) {
+          //printf("face.material_id: %lu\n", static_cast<u64>(mesh.material_ids[faceIdx]));
+
+            if (mesh.num_face_vertices[faceIdx] != TriangleStride) {
                 std::cerr << "ERR: mesh.num_face_vertices != 3  --> " << mesh.num_face_vertices[faceIdx] << '\n';
                 std::cerr << "TRIANGULATE YOUR MESH'es!\n";
                 exit(0);
@@ -172,50 +174,50 @@ int main(const int argc, const char** argv) {
             auto& ib = indices[faceIdx + 1];
             auto& ic = indices[faceIdx + 2];
 
-            const u8 VertStride = 3;
-            const u8 NormStride = 3;
-            const u8 TexStride = 2;
+            const u8 VertexStride  = 3;
+            const u8 NormalStride  = 3;
+            const u8 TextureStride = 2;
             
             auto debugPrint = [&]() 
             {
                 // Uncompress vertices
                 printf("\n"
-                       "va = %f %f %f\n", vertices[ia.vertex_index * VertStride + 0], 
-                                          vertices[ia.vertex_index * VertStride + 1], 
-                                          vertices[ia.vertex_index * VertStride + 2]);
+                       "va = %f %f %f\n", vertices[ia.vertex_index * VertexStride + 0], 
+                                          vertices[ia.vertex_index * VertexStride + 1], 
+                                          vertices[ia.vertex_index * VertexStride + 2]);
 
-                printf("vb = %f %f %f\n",  vertices[ib.vertex_index * VertStride + 0], 
-                                           vertices[ib.vertex_index * VertStride + 1], 
-                                           vertices[ib.vertex_index * VertStride + 2]);
+                printf("vb = %f %f %f\n",  vertices[ib.vertex_index * VertexStride + 0], 
+                                           vertices[ib.vertex_index * VertexStride + 1], 
+                                           vertices[ib.vertex_index * VertexStride + 2]);
 
-                printf("vc =%f %f %f\n", vertices[ic.vertex_index * VertStride + 0], 
-                                         vertices[ic.vertex_index * VertStride + 1], 
-                                         vertices[ic.vertex_index * VertStride + 2]);
+                printf("vc =%f %f %f\n", vertices[ic.vertex_index * VertexStride + 0], 
+                                         vertices[ic.vertex_index * VertexStride + 1], 
+                                         vertices[ic.vertex_index * VertexStride + 2]);
 
                 // Uncompress normals
                 printf("\n"
-                       "na = %f %f %f\n", normals[ia.normal_index * NormStride + 0], 
-                                          normals[ia.normal_index * NormStride + 1], 
-                                          normals[ia.normal_index * NormStride + 2]);
+                       "na = %f %f %f\n", normals[ia.normal_index * NormalStride + 0], 
+                                          normals[ia.normal_index * NormalStride + 1], 
+                                          normals[ia.normal_index * NormalStride + 2]);
 
-                printf("nb = %f %f %f\n",  normals[ib.normal_index * NormStride + 0], 
-                                           normals[ib.normal_index * NormStride + 1], 
-                                           normals[ib.normal_index * NormStride + 2]);
+                printf("nb = %f %f %f\n",  normals[ib.normal_index * NormalStride + 0], 
+                                           normals[ib.normal_index * NormalStride + 1], 
+                                           normals[ib.normal_index * NormalStride + 2]);
 
-                printf("nc =%f %f %f\n", normals[ic.normal_index * NormStride + 0], 
-                                         normals[ic.normal_index * NormStride + 1], 
-                                         normals[ic.normal_index * NormStride + 2]);
+                printf("nc =%f %f %f\n", normals[ic.normal_index * NormalStride + 0], 
+                                         normals[ic.normal_index * NormalStride + 1], 
+                                         normals[ic.normal_index * NormalStride + 2]);
 
                 // Uncompress texture coordinates
                 printf("\n"
-                       "ta = %f %f\n", texcoords[ia.texcoord_index * TexStride + 0], 
-                                       texcoords[ia.texcoord_index * TexStride + 1]);
+                       "ta = %f %f\n", texcoords[ia.texcoord_index * TextureStride + 0], 
+                                       texcoords[ia.texcoord_index * TextureStride + 1]);
 
-                printf("tb = %f %f\n", texcoords[ib.texcoord_index * TexStride + 0], 
-                                       texcoords[ib.texcoord_index * TexStride + 1]);
+                printf("tb = %f %f\n", texcoords[ib.texcoord_index * TextureStride + 0], 
+                                       texcoords[ib.texcoord_index * TextureStride + 1]);
 
-                printf("tc = %f %f\n", texcoords[ic.texcoord_index * TexStride + 0], 
-                                       texcoords[ic.texcoord_index * TexStride + 1]);            
+                printf("tc = %f %f\n", texcoords[ic.texcoord_index * TextureStride + 0], 
+                                       texcoords[ic.texcoord_index * TextureStride + 1]);            
 
 
 
@@ -238,46 +240,46 @@ int main(const int argc, const char** argv) {
             // triangleVertex.a
             overkillVertices[ia.vertex_index] = 
                 OKVertex {
-                    vertices[ia.vertex_index * VertStride + 0],
-                    vertices[ia.vertex_index * VertStride + 1],
-                    vertices[ia.vertex_index * VertStride + 2],
+                    vertices[ia.vertex_index * VertexStride + 0],
+                    vertices[ia.vertex_index * VertexStride + 1],
+                    vertices[ia.vertex_index * VertexStride + 2],
         
-                    normals[ia.normal_index * NormStride + 0],
-                    normals[ia.normal_index * NormStride + 1],
-                    normals[ia.normal_index * NormStride + 2],
+                    normals[ia.normal_index * NormalStride + 0],
+                    normals[ia.normal_index * NormalStride + 1],
+                    normals[ia.normal_index * NormalStride + 2],
 
-                    texcoords[ia.texcoord_index * TexStride + 0],                
-                    texcoords[ia.texcoord_index * TexStride + 1]
+                    texcoords[ia.texcoord_index * TextureStride + 0],                
+                    texcoords[ia.texcoord_index * TextureStride + 1]
                 };
 
             // triangleVertex.b
             overkillVertices[ib.vertex_index] = 
                 OKVertex {
-                    vertices[ib.vertex_index * VertStride + 0],
-                    vertices[ib.vertex_index * VertStride + 1],
-                    vertices[ib.vertex_index * VertStride + 2],
+                    vertices[ib.vertex_index * VertexStride + 0],
+                    vertices[ib.vertex_index * VertexStride + 1],
+                    vertices[ib.vertex_index * VertexStride + 2],
         
-                    normals[ib.normal_index * NormStride + 0],
-                    normals[ib.normal_index * NormStride + 1],
-                    normals[ib.normal_index * NormStride + 2],
+                    normals[ib.normal_index * NormalStride + 0],
+                    normals[ib.normal_index * NormalStride + 1],
+                    normals[ib.normal_index * NormalStride + 2],
 
-                    texcoords[ib.texcoord_index * TexStride + 0],                
-                    texcoords[ib.texcoord_index * TexStride + 1] 
+                    texcoords[ib.texcoord_index * TextureStride + 0],                
+                    texcoords[ib.texcoord_index * TextureStride + 1] 
                 };
 
             // triangleVertex.c
             overkillVertices[ic.vertex_index] = 
                 OKVertex {
-                    vertices[ic.vertex_index * VertStride + 0],
-                    vertices[ic.vertex_index * VertStride + 1],
-                    vertices[ic.vertex_index * VertStride + 2],
+                    vertices[ic.vertex_index * VertexStride + 0],
+                    vertices[ic.vertex_index * VertexStride + 1],
+                    vertices[ic.vertex_index * VertexStride + 2],
         
-                    normals[ic.normal_index * NormStride + 0],
-                    normals[ic.normal_index * NormStride + 1],
-                    normals[ic.normal_index * NormStride + 2],
+                    normals[ic.normal_index * NormalStride + 0],
+                    normals[ic.normal_index * NormalStride + 1],
+                    normals[ic.normal_index * NormalStride + 2],
 
-                    texcoords[ic.texcoord_index * TexStride + 0],                
-                    texcoords[ic.texcoord_index * TexStride + 1]
+                    texcoords[ic.texcoord_index * TextureStride + 0],                
+                    texcoords[ic.texcoord_index * TextureStride + 1]
                 };
 
 
@@ -287,12 +289,101 @@ int main(const int argc, const char** argv) {
                 ib.vertex_index,
                 ic.vertex_index
             });
+        
+        } // END FOR FACES
+
+     
+        // Assuming that all faces across a mesh share the same material.
+        // Maybe this is a dirty assumption, but it seems like all the .obj I 
+        // have seen thus far, only has 1 material per mesh. If you find something
+        // else, please let me know - JSolsvik 08.05.2018
+        printf("materialID = %lu\n", static_cast<u64>( mesh.material_ids[0] ));
+
+        auto& meshMaterial = materials[mesh.material_ids[0]];
+
+        printf("material name = %s\n", meshMaterial.name.c_str() );
+        
+        printf("  material.Ka(mbient) = (%f, %f ,%f)\n",
+           static_cast<const double>(meshMaterial.ambient[0]),
+           static_cast<const double>(meshMaterial.ambient[1]),
+           static_cast<const double>(meshMaterial.ambient[2]));
+
+        printf("  material.Kd(iffuse) = (%f, %f ,%f)\n",
+           static_cast<const double>(meshMaterial.diffuse[0]),
+           static_cast<const double>(meshMaterial.diffuse[1]),
+           static_cast<const double>(meshMaterial.diffuse[2]));
+
+        printf("  material.Ks(pecular) = (%f, %f ,%f)\n",
+           static_cast<const double>(meshMaterial.specular[0]),
+           static_cast<const double>(meshMaterial.specular[1]),
+           static_cast<const double>(meshMaterial.specular[2]));
+
+        printf("  material.Tr(ansmittance) = (%f, %f ,%f)\n",
+           static_cast<const double>(meshMaterial.transmittance[0]),
+           static_cast<const double>(meshMaterial.transmittance[1]),
+           static_cast<const double>(meshMaterial.transmittance[2]));
+
+        printf("  material.Ke(mission) = (%f, %f ,%f)\n",
+           static_cast<const double>(meshMaterial.emission[0]),
+           static_cast<const double>(meshMaterial.emission[1]),
+           static_cast<const double>(meshMaterial.emission[2]));
+
+        printf("  material.Ns(hininess) = %f\n",
+            static_cast<const double>(meshMaterial.shininess));
+
+        printf("  material.Ni(or) = %f\n", 
+            static_cast<const double>(meshMaterial.ior));
+
+
+        printf("  material.dissolve = %f\n",    
+            static_cast<const double>(meshMaterial.dissolve));
+
+        printf("  material.illum(inosity) = %d\n", meshMaterial.illum);
+
+        printf("  material.map_Ka(mbient) = %s\n",   meshMaterial.ambient_texname.c_str());
+        printf("  material.map_Kd(iffuse) = %s\n",   meshMaterial.diffuse_texname.c_str());
+        printf("  material.map_Ks(pecular) = %s\n",   meshMaterial.specular_texname.c_str());
+        printf("  material.map_Ns(pecular_highlight) = %s\n",   meshMaterial.specular_highlight_texname.c_str());
+        printf("  material.map_bump = %s\n", meshMaterial.bump_texname.c_str());
+        
+        printf("    bump_multiplier = %f\n",  static_cast<const double>(meshMaterial.bump_texopt.bump_multiplier));
+
+        printf("  material.map_alpha = %s\n",      meshMaterial.alpha_texname.c_str());
+        printf("  material.disp(lacement) = %s\n", meshMaterial.displacement_texname.c_str());
+
+        // Uknown parameters 
+        std::map<std::string, std::string>::const_iterator it(
+            meshMaterial.unknown_parameter.begin());
+        std::map<std::string, std::string>::const_iterator itEnd(
+            meshMaterial.unknown_parameter.end());
+
+        for (; it != itEnd; it++) {
+          printf("  material.%s = %s\n", it->first.c_str(), it->second.c_str());
         }
+        printf("\n");
+
+    /*
+        PBR = Physically based rendering.. Leaving these features commented out for now, since I want to focus only 
+                on core material properties. Hopefully I will get back to this soon. JSolsvik 08.05.2018
+
+        printf("  <<PBR>>\n");
+        printf("  material.Pr     = %f\n", static_cast<const double>(materials[i].roughness));
+        printf("  material.Pm     = %f\n", static_cast<const double>(materials[i].metallic));
+        printf("  material.Ps     = %f\n", static_cast<const double>(materials[i].sheen));
+        printf("  material.Pc     = %f\n", static_cast<const double>(materials[i].clearcoat_thickness));
+        printf("  material.Pcr    = %f\n", static_cast<const double>(materials[i].clearcoat_thickness));
+        printf("  material.aniso  = %f\n", static_cast<const double>(materials[i].anisotropy));
+        printf("  material.anisor = %f\n", static_cast<const double>(materials[i].anisotropy_rotation));
+        printf("  material.map_Ke = %s\n", materials[i].emissive_texname.c_str());
+        printf("  material.map_Pr = %s\n", materials[i].roughness_texname.c_str());
+        printf("  material.map_Pm = %s\n", materials[i].metallic_texname.c_str());
+        printf("  material.map_Ps = %s\n", materials[i].sheen_texname.c_str());
+        printf("  material.norm   = %s\n", materials[i].normal_texname.c_str());
+    */
+
         overkillMeshes.push_back(overkillMesh);
-    }
 
-
-    printf("vertices count: %lu\n", static_cast<u64>(overkillVertices.size()));
+    } // END FOR MESHES
 
     return 0; 
 }
